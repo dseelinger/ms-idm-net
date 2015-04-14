7dd8d0978e89259e44af66c0b49e730fac9a46ce
Doug Seelinger <dougseelinger@hotmail.com>
2015-04-13 23:03:30 -0400

    Update Setup-Env.ps1

--- Setup-Env.ps1
+++ Setup-Env.ps1
@@ -1,4 +1,4 @@
-[Environment]::SetEnvironmentVariable("MIM_fqdn", "your fully qualified domain name of the fim/mim server(s), like mim.contoso.com", "User") #used for both the FIM Service address and SPN
+[Environment]::SetEnvironmentVariable("MIM_fqdn", "your fully qualified domain name of the Identity Manager server(s), like idm.contoso.com", "User") #used for both the Identity Manager Service address and SPN
 [Environment]::SetEnvironmentVariable("MIM_domain", "contoso", "User")
 [Environment]::SetEnvironmentVariable("MIM_pwd", "whatever your password is", "User")
 [Environment]::SetEnvironmentVariable("MIM_username", "an account that has permissions in Identity Manager", "User")
