4d4f8f1012eccf5654c091e05379be5803572117
Doug Seelinger <dougseelinger@hotmail.com>
2015-04-17 11:40:04 -0400

    Group throroughly unit tested

---
+++ IdmNet/IdmNet.Tests/GroupTests.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using IdmNet.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+// ReSharper disable ObjectCreationAsStatement
+// ReSharper disable UseObjectOrCollectionInitializer
+
+namespace IdmNet.Tests
+{
+    [TestClass]
+    public class GroupTests
+    {
+        [TestMethod]
+        public void It_has_a_paremeterless_constructor()
+        {
+            var it = new Group();
+
+            Assert.AreEqual("Group", it.ObjectType);
+        }
+
+        [TestMethod]
+        public void It_has_a_constructor_that_takes_a_SecurityIdentifierResource_without_a_domain_configuration()
+        {
+            var securityIdentifier = new SecurityIdentifierResource();
+            var it = new Group(securityIdentifier);
+
+            Assert.AreEqual("Group", it.ObjectType);
+        }
+
+        [TestMethod]
+        public void It_has_a_constructor_that_takes_a_SecurityIdentifierResource_WITH_a_domain_configuration()
+        {
+            var domainConfig = new IdmResource { DisplayName = "myDomainConfig" };
+            var securityIdentifier = new SecurityIdentifierResource { DomainConfiguration = domainConfig };
+            var it = new Group(securityIdentifier);
+
+            Assert.AreEqual("Group", it.ObjectType);
+        }
+
+        [TestMethod]
+        public void It_has_a_constructor_that_takes_an_IdmResource()
+        {
+            var baseClass = new SecurityIdentifierResource
+            {
+                DisplayName = "Group DisplayName",
+                Email = "a@b.com",
+                Creator = new Person { DisplayName = "Creator Name", ObjectID = "Creator ObjectID" },
+                DomainConfiguration = new IdmResource { DisplayName = "My Domain Config", ObjectID = "Domain Config ObjectID" }
+
+            };
+            IdmResource idmResource = baseClass;
+            var it = new Group(idmResource);
+
+            Assert.AreEqual("Group", it.ObjectType);
+            Assert.AreEqual("Group DisplayName", it.DisplayName);
+            Assert.AreEqual("a@b.com", it.Email);
+            Assert.AreEqual("Creator Name", it.Creator.DisplayName);
+            Assert.AreEqual("My Domain Config", it.DomainConfiguration.DisplayName);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void It_throws_when_you_try_to_set_ObjectType_to_anything_other_than_Person()
+        {
+            new Group { ObjectType = "foo" };
+        }
+
+        [TestMethod]
+        public void It_returns_null_for_not_present_properties()
+        {
+            var it = new Group();
+
+            Assert.IsNull(it.ComputedMember);
+            Assert.IsNull(it.DisplayedOwner);
+            Assert.IsNull(it.ExplicitMember);
+            Assert.IsNull(it.Filter);
+            Assert.IsNull(it.MembershipAddWorkflow);
+            Assert.IsNull(it.MembershipLocked);
+            Assert.IsNull(it.Owner);
+            Assert.IsNull(it.Scope);
+            Assert.IsNull(it.Temporal);
+            Assert.IsNull(it.msidmDeferredEvaluation);
+        }
+
+        [TestMethod]
+        public void It_can_set_and_get_bool_properties()
+        {
+            var it = new Group
+            {
+                MembershipLocked = true,
+                Temporal = true,
+                msidmDeferredEvaluation = true
+            };
+
+            Assert.AreEqual(true, it.MembershipLocked);
+            Assert.AreEqual(true, it.Temporal);
+            Assert.AreEqual(true, it.msidmDeferredEvaluation);
+        }
+
+        [TestMethod]
+        public void It_can_set_and_get_string_properties()
+        {
+            var it = new Group
+            {
+                Filter = "Test Filter",
+                MembershipAddWorkflow = "Test MembershipAddWorkflow",
+                Scope = "Universal",
+            };
+
+            Assert.AreEqual("Test Filter", it.Filter);
+            Assert.AreEqual("Test MembershipAddWorkflow", it.MembershipAddWorkflow);
+            Assert.AreEqual("Universal", it.Scope);
+        }
+
+        [TestMethod]
+        public void It_can_set_and_get_Owner()
+        {
+            var it = new Group
+            {
+                Owner =
+                    new List<Person>
+                    {
+                        new Person
+                        {
+                            DisplayName = "Person1",
+                            ObjectID = "Person1",
+                            Assistant = new Person {DisplayName = "Assistant1", ObjectID = "Assistant1"}
+                        },
+                        new Person
+                        {
+                            DisplayName = "Person2",
+                            ObjectID = "Person2",
+                            Assistant = new Person {DisplayName = "Assistant2", ObjectID = "Assistant2"}
+                        },
+                    }
+            };
+
+            Assert.AreEqual("Assistant1", it.Owner[0].Assistant.DisplayName);
+            Assert.AreEqual("Assistant2", it.Owner[1].Assistant.DisplayName);
+        }
+
+        [TestMethod]
+        public void It_can_set_and_get_ComputedMember()
+        {
+            var it = new Group
+            {
+                ComputedMember =
+                    new List<SecurityIdentifierResource>
+                    {
+                        new Person
+                        {
+                            DisplayName = "Person1",
+                            ObjectID = "Person1",
+                            Assistant = new Person {DisplayName = "Assistant1", ObjectID = "Assistant1"}
+                        },
+                        new Person
+                        {
+                            DisplayName = "Person2",
+                            ObjectID = "Person2",
+                            Assistant = new Person {DisplayName = "Assistant2", ObjectID = "Assistant2"}
+                        },
+                    }
+            };
+
+            Assert.AreEqual("Assistant1", ((Person)(it.ComputedMember[0])).Assistant.DisplayName);
+            Assert.AreEqual("Assistant2", ((Person)(it.ComputedMember[1])).Assistant.DisplayName);
+        }
+
+        [TestMethod]
+        public void It_can_set_and_get_ExplicitMember()
+        {
+            var it = new Group
+            {
+                ExplicitMember =
+                    new List<SecurityIdentifierResource>
+                    {
+                        new Person
+                        {
+                            DisplayName = "Person1",
+                            ObjectID = "Person1",
+                            Assistant = new Person {DisplayName = "Assistant1", ObjectID = "Assistant1"}
+                        },
+                        new Person
+                        {
+                            DisplayName = "Person2",
+                            ObjectID = "Person2",
+                            Assistant = new Person {DisplayName = "Assistant2", ObjectID = "Assistant2"}
+                        },
+                    }
+            };
+
+            Assert.AreEqual("Assistant1", ((Person)(it.ExplicitMember[0])).Assistant.DisplayName);
+            Assert.AreEqual("Assistant2", ((Person)(it.ExplicitMember[1])).Assistant.DisplayName);
+        }
+
+        [TestMethod]
+        public void It_can_set_and_get_DisplayedOwner()
+        {
+            var it = new Group
+            {
+                DisplayedOwner =
+                    new Person
+                    {
+                        DisplayName = "Person1",
+                        ObjectID = "Person1",
+                        Assistant = new Person {DisplayName = "Assistant1", ObjectID = "Assistant1"}
+                    },
+            };
+
+            Assert.AreEqual("Assistant1",it.DisplayedOwner.Assistant.DisplayName);
+        }
+    }
+}
--- IdmNet/IdmNet/Models/Group.cs
+++ IdmNet/IdmNet/Models/Group.cs
@@ -1,23 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-
 // ReSharper disable InconsistentNaming
 
 namespace IdmNet.Models
 {
+    /// <summary>
+    /// Group class - covers both Security Groups and Distribution Lists
+    /// </summary>
     public class Group : SecurityIdentifierResource
     {
-        private List<IdmResource> _computedMembers;
         private Person _displayedOwner;
-        private List<IdmResource> _explicitMembers;
         private List<Person> _owners;
+        private List<SecurityIdentifierResource> _computedMembers;
+        private List<SecurityIdentifierResource> _explicitMembers;
 
+        /// <summary>
+        /// Parameterless CTOR
+        /// </summary>
         public Group()
         {
             ObjectType = ForcedObjType = "Group";
         }
 
+        /// <summary>
+        /// Build a Group object from a SecurityIdentifierResource object
+        /// </summary>
+        /// <param name="resource">base class</param>
         public Group(SecurityIdentifierResource resource)
             : base(resource)
         {
@@ -51,24 +60,38 @@ namespace IdmNet.Models
             }
         }
 
+        /// <summary>
+        /// List of owners of the Group
+        /// </summary>
         public List<Person> Owner
         {
             get { return GetMultiValuedAttr("Owner", _owners); }
             set { SetMultiValuedAttr("Owner", out _owners, value); }
         }
 
-        public List<IdmResource> ComputedMember
+        /// <summary>
+        /// Resources in the set that are computed from the membership filter.  List of resources that are currently 
+        /// computed as being members of the group. Should only be retrieved from Identity Manger.  Use ExplicitMember
+        /// to manually add members to the group.
+        /// </summary>
+        public List<SecurityIdentifierResource> ComputedMember
         {
             get { return GetMultiValuedAttr("ComputedMember", _computedMembers); }
             set { SetMultiValuedAttr("ComputedMember", out _computedMembers, value); }
         }
 
+        /// <summary>
+        /// Determines when evaluation of the group happens with respect to request processing - real-time or deferred.
+        /// </summary>
         public bool? msidmDeferredEvaluation
         {
             get { return AttrToBool("msidmDeferredEvaluation"); }
             set { SetAttrValue("msidmDeferredEvaluation", value.ToString()); }
         }
 
+        /// <summary>
+        /// Single Person who is listed as the owner of the group
+        /// </summary>
         public Person DisplayedOwner
         {
             get { return GetAttr("DisplayedOwner", _displayedOwner); }
@@ -79,18 +102,28 @@ namespace IdmNet.Models
             }
         }
 
+        /// <summary>
+        /// A predicate defining a subset of the resources.
+        /// </summary>
         public string Filter
         {
             get { return GetAttrValue("Filter"); }
             set { SetAttrValue("Filter", value); }
         }
 
-        public List<IdmResource> ExplicitMember
+        /// <summary>
+        /// (aka Manually-managed Membership) Members in the group that are manually managed.
+        /// </summary>
+        public List<SecurityIdentifierResource> ExplicitMember
         {
             get { return GetMultiValuedAttr("ExplicitMember", _explicitMembers); }
             set { SetMultiValuedAttr("ExplicitMember", out _explicitMembers, value); }
         }
 
+        /// <summary>
+        /// Membership Add Workflow - Workflow that fires when someone is manually added to the group. Note that this
+        /// is a string and not a reference object in the Identity Manager Schema.
+        /// </summary>
         [Required]
         public string MembershipAddWorkflow
         {
@@ -98,6 +131,9 @@ namespace IdmNet.Models
             set { SetAttrValue("MembershipAddWorkflow", value); }
         }
 
+        /// <summary>
+        /// If true, no one may be manually added to the group.
+        /// </summary>
         [Required]
         public bool? MembershipLocked
         {
@@ -105,6 +141,9 @@ namespace IdmNet.Models
             set { SetAttrValue("MembershipLocked", value.ToString()); }
         }
 
+        /// <summary>
+        /// Group scope.  Must be one of (DomainLocal|Global|Universal)
+        /// </summary>
         [RegularExpression("^(DomainLocal|Global|Universal)$")]
         [Required]
         public string Scope
@@ -113,6 +152,9 @@ namespace IdmNet.Models
             set { SetAttrValue("Scope", value); }
         }
 
+        /// <summary>
+        /// Defined by a filter that matches resources based on date and time attributes
+        /// </summary>
         public bool? Temporal
         {
             get { return AttrToBool("Temporal"); }
