0618c28723aa17cf02f871c4bcb2e0f023d5cf34
Doug Seelinger <dougseelinger@hotmail.com>
2015-05-15 12:01:48 -0400

    Added a couple of missed files

---
+++ IdmNet/IdmNet/Models/Schema.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace IdmNet.Models
+{
+    /// <summary>
+    /// An object type description with Binding information associated with it.
+    /// </summary>
+    public class Schema : ObjectTypeDescription
+    {
+        /// <summary>
+        /// Parameterless Constructor
+        /// </summary>
+        public Schema()
+        {
+            BindingDescriptions = new List<BindingDescription>();
+        }
+
+        /// <summary>
+        /// Parameterized
+        /// </summary>
+        public Schema(IdmResource resource) : base(resource)
+        {
+            BindingDescriptions = new List<BindingDescription>();
+        }
+
+        /// <summary>
+        /// List of associated BindingDescriptions 
+        /// </summary>
+        public List<BindingDescription> BindingDescriptions { get; set; }
+    }
+}
